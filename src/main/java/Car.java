0c078834d5f34b43e1be248d0a11c4180950cdd7
Oleg Chvirov <wowgameoleg@mail.ru>
2021-01-21 15:33:13 +0300

    homework completed 2

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class Car {
+import java.util.concurrent.CountDownLatch;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    private Race race;
+    private int speed;
+    private String name;
+    public static long maxCount = MainClass.countDownLatchFinish.getCount();
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            MainClass.cdlCountDown();
+
+            //Когда все машины готовы - объявляем СТАРТ
+            if (MainClass.countDownLatch.getCount() == 0) {
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //ждем когда все машины будут готовы
+        MainClass.cdlAwait();
+
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+
+        //когда все фрагменты трассы пройдены - уменьшаем count у лэтча, ответственного за финиш
+                MainClass.countDownLatchFinish.countDown();
+
+        //если закончившая трассу машина была 1-ой - печатаем WIN
+        if (MainClass.countDownLatchFinish.getCount() == (maxCount - 1)) {
+            System.out.println(this.name + " WIN");
+        }
+
+
+    }
 }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class MainClass {
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+    public static final CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT); // Лэтч для старта
+    public static final CountDownLatch countDownLatchFinish = new CountDownLatch(CARS_COUNT); // Лэтч для финиша
+    public static final Semaphore semaphore = new Semaphore(CARS_COUNT/2); //Семафор для тоннеля
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread((cars[i])).start();
+        }
+
+
+
+        //Когда все участники завершают трассу - объявляем КОНЕЦ
+        try {
+            MainClass.countDownLatchFinish.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+
+    public static void cdlCountDown() {
+        countDownLatch.countDown();
+    }
+
+    public static void cdlAwait() {
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void smpAcquire() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void smpRelease() {
+        semaphore.release();
+    }
 }
+
+
+
+
+
